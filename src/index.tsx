f0dbb3d74f81536c2c05093d01ac300cdef296b4
Ryland Goldstein <ryland@binaris.com>
2019-12-22 20:33:48 +0000

    do work for boilerplate setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
+
+import { BrowserRouter } from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Router from './Router';
+
+import './styles/index.scss';
+
+ReactDOM.render((
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
